4d580826a1d9681b14dbec4c875d15b3c5d72429
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-15 14:13:40 +0100

    add ReceiverPriority enum defining receiver urgency levels with required bag count

--- src/main/java/com/bloodbank/model/entities/ReceiverPriority.java
+++ src/main/java/com/bloodbank/model/entities/ReceiverPriority.java
@@ -1,5 +1,23 @@
 package com.bloodbank.model.entities;
 
-public class ReceiverPriority {
-
+public enum ReceiverPriority {
+    CRITIQUE("Critique", 4),
+    URGENT("Urgent", 3),
+    NORMAL("Normal", 1);
+    
+    private final String label;
+    private final int requiredBags;
+    
+    ReceiverPriority(String label, int requiredBags) {
+        this.label = label;
+        this.requiredBags = requiredBags;
+    }
+    
+    public String getLabel() {
+        return label;
+    }
+    
+    public int getRequiredBags() {
+        return requiredBags;
+    }
 }
