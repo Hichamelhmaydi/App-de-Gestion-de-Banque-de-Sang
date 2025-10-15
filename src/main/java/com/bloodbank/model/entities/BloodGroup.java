3fc8c67116ae049b9250251d4d2c3aef7d5bcfc1
Hichamelhmaydi <ct1594065@gmail.com>
2025-10-15 11:48:19 +0100

    add BloodGroup enum defining all blood types  with string conversion support

--- src/main/java/com/bloodbank/model/entities/BloodGroup.java
+++ src/main/java/com/bloodbank/model/entities/BloodGroup.java
@@ -1,5 +1,31 @@
 package com.bloodbank.model.entities;
 
-public class BloodGroup {
-
+public enum BloodGroup {
+    O_POSITIVE("O+"),
+    O_NEGATIVE("O-"),
+    A_POSITIVE("A+"),
+    A_NEGATIVE("A-"),
+    B_POSITIVE("B+"),
+    B_NEGATIVE("B-"),
+    AB_POSITIVE("AB+"),
+    AB_NEGATIVE("AB-");
+    
+    private final String symbol;
+    
+    BloodGroup(String symbol) {
+        this.symbol = symbol;
+    }
+    
+    public String getSymbol() {
+        return symbol;
+    }
+    
+    public static BloodGroup fromString(String text) {
+        for (BloodGroup group : BloodGroup.values()) {
+            if (group.symbol.equals(text)) {
+                return group;
+            }
+        }
+        return null;
+    }
 }
